58c8a4efe83114ec3238966c4596b75cc5412119
Rowell Verdejo <rowellverdejo@gmail.com>
2023-05-18 09:43:16 +0800

    Finalize joke functionality

--- script.js
+++ script.js
@@ -111,17 +111,38 @@ const VoiceRSS = {
     },
 };
 
-const test = () => {
+// Passing Jokes to VoiceRSS
+const tellMe = (joke) => {
     VoiceRSS.speech({
         key: "01adf527c9a34e17a7373f5031fd1cdd",
-        src: "Hello, world!",
+        src: joke,
         hl: "en-us",
-        v: "Linda",
+        v: "John",
         r: 0,
         c: "mp3",
         f: "44khz_16bit_stereo",
         ssml: false,
     });
+    console.log(joke);
 };
 
-test();
+// Get Jokes from Joke API
+const getJokes = async () => {
+    let joke = "";
+    const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark";
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        tellMe(joke);
+    } catch (error) {
+        console.log("Haiyaaah!", error);
+    }
+};
+
+// Joke Event Listener
+button.addEventListener("click", getJokes);
